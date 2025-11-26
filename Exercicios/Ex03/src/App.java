7d7011f036f40edf19190fef3eddad6a4005053e
Fernando Souza <fernandofvsouza@icloud.com>
2025-11-27 00:06:39 +0100

    ex03

---
+++ Exercicios/Ex03/src/App.java
@@ -0,0 +1,21 @@
+/*
+A padaria hotpão vende uma certa quantidade de pães franceses e uma quantidade de broas a cada dia. Cada pão francês custa R$ 0,12 e a broa custa R$ 1,20. Ao final do dia, o dono quer saber quanto arrecadou com a venda dos pães e broas, faça um algoritmo que leia a quantidade de pães e broas vendidas, em um dia e calcule o total arrecadado com a venda dos mesmos.
+*/
+import java.util.Scanner;
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        int qtdBroa, qtdPao;
+        float total;
+
+        
+
+        
+        
+
+    }
+}
