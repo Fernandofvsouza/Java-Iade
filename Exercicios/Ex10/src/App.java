72e52d31b4296f09ce9e3d7f2391d620a509abb7
Fernando souza <fernandofvsouza@icloud.com>
2025-12-17 11:23:44 +0000

    ex10

---
+++ Exercicios/Ex10/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//Faça um algoritmo que leia a idade de uma pessoa expressa em anos meses e dias e mostre-a expressa em dias. leve em consideração o ano com 365 dias e o mes com 30
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int idade;
+        float idadeEmDias;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Qual a sua idade? ");
+        input.idade = 
+
+    }
+}
