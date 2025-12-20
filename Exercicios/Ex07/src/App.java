b1d82c81f8fa9f570188495cd8b92f3cdc436282
Fernando souza <fernandofvsouza@icloud.com>
2025-12-20 16:51:40 +0000

    update

--- Exercicios/Ex07/src/App.java
+++ Exercicios/Ex07/src/App.java
@@ -14,38 +14,43 @@ exibidas informações sobre as demais cédulas):
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        float valor;
+        int valor;
         System.out.print("Informe o valor em euros: ");
-        valor = input.nextFloat();
-        float notas100, notas50, notas20, notas10, notas5;
-        notas100 = (int) (valor / 100);
-        valor = valor % 100;    
-        notas50 = (int) (valor / 50);
+        valor = input.nextInt();
+        
+        int notas100, notas50, notas20, notas10, notas5;
+        
+        notas100 = valor / 100;
+        valor = valor % 100;
+        
+        notas50 = valor / 50;
         valor = valor % 50;
-        notas20 = (int) (valor / 20);
+        
+        notas20 = valor / 20;
         valor = valor % 20;
-        notas10 = (int) (valor / 10);
+        
+        notas10 = valor / 10;
         valor = valor % 10;
-        notas5 = (int) (valor / 5);
+        
+        notas5 = valor / 5;
         valor = valor % 5;
+        
         if (notas100 > 0){
-            System.out.println((int)notas100 + " nota(s) de 100 euros.");
+            System.out.println(notas100 + " nota(s) de 100 euros.");
         }
         if (notas50 > 0){
-            System.out.println((int)notas50 + " nota(s) de 50 euros.");
+            System.out.println(notas50 + " nota(s) de 50 euros.");
         }
         if (notas20 > 0){
-            System.out.println((int)notas20 + " nota(s) de 20 euros.");
+            System.out.println(notas20 + " nota(s) de 20 euros.");
         }
         if (notas10 > 0){
-            System.out.println((int)notas10 + " nota(s) de 10 euros.");
+            System.out.println(notas10 + " nota(s) de 10 euros.");
         }
         if (notas5 > 0){
-            System.out.println((int)notas5 + " nota(s) de 5 euros.");
+            System.out.println(notas5 + " nota(s) de 5 euros.");
         }
+        
         input.close();
-
-
-
     }
 }
